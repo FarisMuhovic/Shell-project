7bc6153f213ff55140864344b6774714b6edfd75
Faris Muhovic <faris.fare123456@gmail.com>
2024-03-19 13:44:12 +0100

    aaaaaaaaaa

--- utils.h
+++ utils.h
@@ -1,7 +1,34 @@
 #pragma once
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
-inline void pprompt(){
-	printf("%s@%s:%s> ", getlogin(), "machina", getcwd(NULL, 0) );
+static inline void pprompt(){
+	char path[128];
+	getcwd(path, sizeof(path));
+	char machinenamef[64];
+	gethostname(machinenamef, 64);
+	char newpath[128];
+
+	printf("%ld\n", strlen(path));
+
+	int slashcount = 0;
+	for (int i = 0; i < strlen(path); i++)
+	{
+
+		if (path[i] == 47) {
+			slashcount++;
+		}
+
+		if (slashcount == 4) {
+			// printf("%c\n", path[i]);
+			// strcat(newpath, path[i]);
+			newpath[i + slashcount] = path[i];
+			newpath[i+ slashcount + 1] = '\0';
+			printf("%s", newpath);
+		}
+	}
+	printf("%i\n", slashcount);
+	printf("%s", newpath);
+	// printf("%s@%s:%s> ", getlogin(),  machinenamef , newpath);
 }
