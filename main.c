247b7ed2a4caeaa2f9a2a8644a7eb5651684a69d
Muha0644 <muhamedmulic9@gmail.com>
2024-03-19 11:39:34 +0100

    fix main

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include "defines.h"
 
 const char SPACE[4] = " ";
 
 int main() {
-	printf("WELCOME!\n");
-	while (1) {	
-		printf("> ");
-		char buffer[1024]; 
+	printf(WELCOME_MSG);
+	char buffer[1024]; 
+	while(1){	
+		printf(PROMPT);
 		fgets(buffer, 1024, stdin);
+		
 		char* token = strtok(strtok(buffer, "\n"), SPACE);
-	
+
 		if(strcmp(token, "exit") == 0) {
 			return 0;
 		}
