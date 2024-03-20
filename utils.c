d8550720ab8e7dc1868013f8e5f4b6700ec32ac2
Muha0644 <muhamedmulic9@gmail.com>
2024-03-20 09:48:45 +0100

    move pprompt to utils.c, as its too big to be inline

--- utils.c
+++ utils.c
@@ -3,13 +3,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+void pprompt(){
+	char path[128];
+	getcwd(path, sizeof(path));
+	char machinenamef[64];
+	gethostname(machinenamef, 64);
+	char newpath[128];
+
+	int slashcount = 0;
+	for (int i = 0; i < strlen(path); i++){
+		if (path[i] == '/') {
+			slashcount++;
+		}
+		//slashcount += (path[i] == '/')
+
+		if (slashcount == 4) {
+			// printf("%c\n", path[i]);
+			// strcat(newpath, path[i]);
+			newpath[i + slashcount] = path[i];
+			newpath[i+ slashcount + 1] = '\0';
+			//printf("%s", newpath);
+		}
+	}
+	//printf("%i\n", slashcount);
+	//printf("%s", newpath);
+	printf("%s@%s:%s> ", getlogin(),  machinenamef , newpath);
+}
+
 struct Arguments buffer2Vector(char *buffer){
 	int navodnik = 0; //`'"
 	int argc = 0;
 	for(int i=0; i!=strlen(buffer); i++){ //while true?
 		if(buffer[i] == '\n') break;
 		if(buffer[i] == ' ' && navodnik == 0){
-			buffer[i] = (char) NULL;
+			buffer[i] = '\0';
 		} else if (buffer[i] == '\"'){
 			navodnik ^= 0b001;	
 		} else if (buffer[i] == '\''){
@@ -23,8 +50,9 @@ struct Arguments buffer2Vector(char *buffer){
 }
 
 int echo(struct Arguments in){
-	printf("\nargc: %d", in.argc);
-	for(int i=0; i!=in.argc; i++){
-		printf("%s",in.argv[i]);
+	printf("argc: %d\n", in.argc);
+	for(int i=0; i<in.argc; i++){
+		printf("%s\n",in.argv[i]);
 	}
+	return 0;
 }
--- utils.h
+++ utils.h
@@ -8,35 +8,7 @@ struct Arguments{
 	char** argv;
 };
 
-static inline void pprompt(){
-	char path[128];
-	getcwd(path, sizeof(path));
-	char machinenamef[64];
-	gethostname(machinenamef, 64);
-	char newpath[128];
-
-	printf("%ld\n", strlen(path));
-
-	int slashcount = 0;
-	for (int i = 0; i < strlen(path); i++)
-	{
-
-		if (path[i] == 47) {
-			slashcount++;
-		}
-
-		if (slashcount == 4) {
-			// printf("%c\n", path[i]);
-			// strcat(newpath, path[i]);
-			newpath[i + slashcount] = path[i];
-			newpath[i+ slashcount + 1] = '\0';
-			printf("%s", newpath);
-		}
-	}
-	printf("%i\n", slashcount);
-	printf("%s", newpath);
-	printf("%s@%s:%s> ", getlogin(),  machinenamef , newpath);
-}
+void pprompt();
 
 int echo(struct Arguments);
 
