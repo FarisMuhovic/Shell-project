9a59e815553e3df60bb5f64aae0453a5de080199
Muha0644 <muhamedmulic9@gmail.com>
2024-03-20 12:42:35 +0100

    begin task 1.5

---
+++ task1.5.c
@@ -0,0 +1,16 @@
+#pragma once
+
+void question151(){
+	printf("Bboth require syscalls, which run in kernel mode. "
+		"This is because peripheral devices like the hardware clock "
+		"and external storage are managed by the OS, and are not "
+		"available to user mode programs. ");
+}
+
+void question152(){
+	printf("");
+}
+
+void question153(){
+	printf("");
+}
